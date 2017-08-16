0ed4348a41ed8119fd704bea7b7a4867a8959229
安爷 <chenjunan@foxmail.com>
2017-08-16 12:53:22 +0800

    Delete sysAuthority.java

--- shiro/src/main/java/com/stip/net/entity/sysAuthority.java
+++
@@ -1,7 +0,0 @@
-package com.stip.net.entity;
-
-import com.stip.net.entity.base.BsysAuthority;
-
-public class sysAuthority extends BsysAuthority {
-    private static final long serialVersionUID = 1L;
-}
