137e329fe4738b173965d435f029d376d209bffe
安爷 <chenjunan@foxmail.com>
2017-08-16 13:30:55 +0800

    mybatis生成依赖添加

---
+++ shiro/src/main/java/com/stip/net/dao/UserDao.java
@@ -0,0 +1,11 @@
+package com.stip.net.dao;
+
+import com.stip.mybatis.generator.plugin.GenericMapper;
+import com.stip.net.entity.User;
+import com.stip.net.entity.UserExample;
+
+ /**
+ * generator XML
+ **/
+public interface UserDao extends GenericMapper<User, UserExample, Integer> {
+}
